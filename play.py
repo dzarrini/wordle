650ca190591f850ea8eebf1f340fb457cf793225
Dan Zarrini <daniel.zarrini@gmail.com>
2024-08-17 12:35:00 +0900

    Exp 0: this is wrong.

--- play.py
+++ play.py
@@ -1,51 +1,15 @@
 #!/usr/bin/python3
 
 import sys
-from wordle import best_word, answer_wordlist
+from wordle import best_word, answer_wordlist, guess
 
 W_LENGTH = 5
 
-def accept(color, word, candidate, word_length=W_LENGTH):
-  i = 0
-  j = 0
 
-  char_map = [-1] * 26
-  for i in range(word_length):
-    c = color[i]
-    char = word[i]
-    if c == "G":
-      if char != candidate[i]:
-        return False
-      char_map[ord(char) - ord('a')] = i
-    elif c == "Y":
-      j = char_map[ord(char) - ord('a')] + 1
-      y_match = False
-      while j < W_LENGTH:
-        if i == j or (j < word_length and color[j] == 'G'):
-          j = j + 1
-          continue
-        if candidate[j] == char:
-          y_match = True
-          break
-        j = j + 1
-      if y_match is False:
-        return False
-      char_map[ord(char) - ord('a')] = j
-    elif c == "R":
-      j = char_map[ord(char) - ord('a')] + 1
-      while j < word_length:
-        if color[j] == 'G':
-          j = j + 1
-          continue
-        if candidate[j] == char:
-          return False
-        j = j + 1
-  return True
-
-def guess(color, word, candidates):
-  def accept_(candidate):
-    return accept(color, word, candidate)
-  return list(filter(accept_, candidates))
+# print(accept('RRYRY', 'speed', 'abide'))
+# print(accept('YRYYR', 'speed', 'erase'))
+# print(accept('GRGRR', 'speed', 'steal'))
+# print(accept('RYGYR', 'speed', 'crepe'))
 
 def pick_word(rst, available_words):
   for i in range(1, len(rst)):
@@ -77,4 +41,5 @@ def play():
       sys.exit(0)
 
 if __name__ == '__main__':
-  play()
+  # play()
+  sys.exit(0)
